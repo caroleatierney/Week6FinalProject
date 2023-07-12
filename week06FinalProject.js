ab2ad20834dda0cb3ac8aeb00a88768adf3c002c
carole tierney <caroleatierney@gmail.com>
2023-07-11 21:55:38 -0400

    Updates after mentor meeting

--- week06FinalProject.js
+++ week06FinalProject.js
@@ -1,22 +1,87 @@
-class Card {
+class Deck { // create initial deck
     constructor(suit, value, character) {
-        this.suit = suit;
-        this.value = value;
-        this.character = character;
+       let cardDeck = [];
+       let suits = ['spade', 'heart', 'diamond', 'club'];
+
+        for (let i = 0; i < suits.length; i++) { // loop through suits array
+            let character = '';
+            for (let j = 2; j <= 14; j++) { // loop through values array
+                // character = () => {
+                //     switch (j) {
+                //         case 11:
+                //             return 'Joker'
+                //         case 12:
+                //             return 'Queen'
+                //         case 13:
+                //             return 'King'
+                //         case 14:
+                //             return 'Ace'
+                //         default:
+                //             return 'j'
+                //     }
+                // }
+
+
+                    switch (j) {
+                        case 11:
+                            character = 'Joker'
+                        case 12:
+                            character = 'Queen'
+                        case 13:
+                            character = 'King'
+                        case 14:
+                            character = 'Ace'
+                        default:
+                            character = j
+                    }
+
+                let card = { // create a card object to push onto the deck array
+                    suit: suits[i],
+                    value: j,
+                    character: character
+                }
+
+                cardDeck.push(this.card);
+                console.log("deck: " + deck.cardDeck);
+            }
+        }
+
+        // shuffle deck
+        // Math.random gives a floating point >0 and <1
+        // Math.floor gives you a rounded up whole number
+        // Multiplying by array length will give you an index from 0 to the length of the array
+        // This is a the Fisher Yates Method for shuffling an array
+        // It shuffles the list in place starting with the last element of the array
+        // The last item becomes a random index value and the random index becomes what was in i, then it repeats with the next index to the left and so on till it reaches iindex 0
+        for (let k = deck.cardDeck.length; k > 0; k--) {
+            var randomIndex = Math.floor(Math.random) * (k + 1)
+            var hold = cardDeck[k]; // save original value for swap
+            cardDeck[k] = cardDeck[randomIndex] // make current value of i = the random index's value
+            cardDeck[randomIndex] = hold // make value of the randomIndex equal to the original value of i
+        }
     }
 }
 
+// player related fields & methods
 class Players {
     constructor(points) {
         this.player1TotalPoints = 0;
         this.player2TotalPoints = 0;
     }
 
+    compareCards(one, two) {
+        if (one != two && one > two) {  // compare cards and update winner by one point - do nothing if cards are equal
+            players.addPoints(one);
+        } else if (one != two && two > one) {
+            players.addPoints(two);
+        }
+    }
+
     addPoints(player) {
         if (player == one) {
-            this.player1TotalPoints ++;
+            this.player1TotalPoints++;
         } else {
-            this.player2TotalPoints ++;
+            this.player2TotalPoints++;
         }
     }
 
@@ -29,77 +94,20 @@ class Players {
     }
 }
 
-class Deck extends Card {
-    constructor() {
-        super(suit);
-        super(value);
-        super(character);
-        this.cardsArray = [];
-    }
+// main game logic
+let deck = new Deck; // create new instance of Deck
+let players = new Players;
+// console.log("deck: " + deck.cardDeck);
+// console.log("array length: " + deck.cardDeck.length);
+// while (deck.cardDeck.length > 0) { // as long as there are cards in the deck
+//     console.log("array length: " + deck.cardDeck.length);
+//     let one = deck.cardDeck.pop; // deal a card to player 1
+//     let two = deck.cardDeck.pop; // deal a card to player 2
+//     console.log("one: " + one + ", two" + two);
+//     deck.compareCards(one, two); // execute player compare method
+// }
 
-    // create initial deck
-    newDeck () {
-        let suit = '';
-        let character = '';
-        let value = 0;
-        const suits = ['spade', 'heart', 'diamond', 'club'];
-        for (let i = 0; i < suits.length; i++) {
-            for (let j = 2; j <= 14; j++) {
-                suit = suits[i];
-                value = j;
-                switch (j) {
-                    case 11:
-                        character = 'Joker'
-                        break;
-                    case 12:
-                        character = 'Queen'
-                        break;
-                    case 13:
-                        character = 'King'
-                        break;
-                    case 14:
-                        character = 'Ace'
-                        break;
-                    default:
-                        character = 'j'
-                        break;
-                }
-                let card = new Card(suit, value, character)
-                const square = new Rectangle(10, 10);
-                this.cardsArray.push
-            }
-        }
-    }
-
-    // shuffle deck
-    shuffleDeck () {
-        // tbd!!!
-    }
-
-    // dealHands
-    dealHands () {
-        let players = new Players;
-        while (this.cardsArray.length > 0) { // as long as there are cards in the deck
-            let one = this.cardsArray.pop; // deal a card to player 1
-            let two = this.cardsArray.pop; // deal a card to player 2
-            this.compareCards(one, two) // execute player compare method
-        }
-
-        // execute players determine winner method and display winner and total points for each player
-        alert(`The winner is: ${players.detWinner()}!
-               Player 1 had ${players.player1TotalPoints} and
-               player 2 had ${players.player2TotalPoints}`)
-    }
-
-    compareCards() {
-        if (one > two) { // compare cards and update winner by one point - do nothing if cards are equal
-            players.addPoints(one);
-        } else if (two > one) {
-            players.addPoints(two);
-        }
-    }
-}
-
-deck = new Deck; // create new instance of deck
-deck.newDeck(); // create actual deck
-deck.dealHands(); // play the game
+// execute players determine winner method and display winner and total points for each player
+alert(`The winner is: ${players.detWinner()}!
+            Player 1 had ${players.player1TotalPoints} and
+            player 2 had ${players.player2TotalPoints}`);
